fc3e0b0f14839f121f92cee64dfc2a8013d6f1dd
John Grismore <jrgrismore@cableone.net>
2019-01-23 20:11:50 -0600

    Initial commit

--- OW_Mobile/AppDelegate.swift
+++ OW_Mobile/AppDelegate.swift
@@ -13,8 +13,6 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
-
-//intial commit comment
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
