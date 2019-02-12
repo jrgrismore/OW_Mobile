a3010a7d97e07099868cb1b022f7efd13ad3ca6d
John Grismore <jrgrismore@cableone.net>
2019-02-11 18:08:24 -0600

    Removed ? from Unneeded directory.

---
+++ OW_Mobile/Unneeded/OWCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  OWCollectionViewCell.swift
+//  OW_Mobile
+//
+//  Created by John Grismore on 1/30/19.
+//  Copyright © 2019 John Grismore. All rights reserved.
+//
+
+import UIKit
+
+class OWCollectionViewCell: UICollectionViewCell
+{
+    
+  @IBOutlet weak var owCellImg: UIImageView!
+}
