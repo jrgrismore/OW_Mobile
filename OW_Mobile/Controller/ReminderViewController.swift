1c39848d821536c2123de61122348051f1bc2d4b
John Grismore <jrgrismore@cableone.net>
2019-03-20 10:53:49 -0500

    Improved Reminders screen compatibility.

--- OW_Mobile/Controller/ReminderViewController.swift
+++ OW_Mobile/Controller/ReminderViewController.swift
@@ -11,14 +11,18 @@ import UIKit
 class ReminderViewController: UIViewController
 {
   @IBOutlet weak var alertsView: UIView!
-  @IBOutlet weak var alertsTimeView: UIView!
+  @IBOutlet weak var alert30MinView: UIView!
+  @IBOutlet weak var alert1HrView: UIView!
+  @IBOutlet weak var alert2HrView: UIView!
   @IBOutlet weak var prevDayView: UIView!
   
   override func viewDidLoad()
   {
     super.viewDidLoad()
     self.alertsView.layer.cornerRadius = 10
-    self.alertsTimeView.layer.cornerRadius = 10
+    self.alert30MinView.layer.cornerRadius = 10
+    self.alert1HrView.layer.cornerRadius = 10
+    self.alert2HrView.layer.cornerRadius = 10
     self.prevDayView.layer.cornerRadius = 10
   }
   
